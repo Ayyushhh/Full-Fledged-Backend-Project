ad313d1e530e2bbeaf7b28066fc899db6468c1f2
Ayyushhh <ayushj820@gmail.com>
2024-04-01 23:25:38 +0530

    file upload config setting

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath){
+            console.log("File path not there")
+            return NULL
+        }
+        //for upload on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file uploaded successfully
+        console.log("File is uploaded on cloudinary",response.url);
+        return response
+    }
+    catch (error){
+        //removing saved temparory files as the uploaded got failed
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
