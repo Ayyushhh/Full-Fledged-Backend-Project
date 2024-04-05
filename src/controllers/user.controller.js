f4cfff5db0b3203062b859883a28b28a40beb66f
Ayyushhh <ayushj820@gmail.com>
2024-04-05 18:16:37 +0530

    Login and logout controller and middleware for auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async(userID) => {
+    try{
+        const user = await User.findById(userID)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        //refresh Token ko database mai kaise daale
+        user.refreshToken = refreshToken
+        //now save krdo
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken,refreshToken}
+
+    }
+    catch(error){
+        throw new ApiError(500,"Something went wrong while generating Access and Refresh Token")
+    }
+}
+
 
 //method hai ye 
 const registerUser = asyncHandler( async (req,res) => {
@@ -87,4 +106,68 @@ const registerUser = asyncHandler( async (req,res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler( async (req,res) => {
+    // req body se data lekr aao (User ka input data)
+    const {email, username, password} = req.body
+    // username or email 
+    if(!email || !username){
+        throw new ApiError(400,"Email or Username is required")
+    }
+    // find the user
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404,"User not exist")
+    }
+    // password check
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(401,"Password Incorrect")
+    }
+    // access and refresh token generate
+        //method bana lo for easy to use
+    const {accessToken,refreshToken} = await generateAccessAndRefreshToken(user._id)
+    
+    //optional ki agar koi filed remove krni h kya user ko 
+    const loggedInUsr = await User.findById(user._id).select("-password -refreshToken")
+
+    // send krdo ye dono ko cookie mai
+    const options = {
+        //server se hi modify krskte h not from frontend
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(200,{
+            user: loggedInUsr, accessToken, refreshToken
+        },"User Logged in Successfully")
+    )
+})
+
+//logout 
+const logoutUser = asyncHandler(async(req,res) => {
+    //to logout remove or reset cookies first 
+    await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            refreshToken:undefined
+        }
+    })
+
+    const options = {
+        //server se hi modify krskte h not from frontend
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearcookie("accessToken",options)
+    .clearcookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User Logged out"))
+})
+
+export {registerUser,loginUser,logoutUser}
